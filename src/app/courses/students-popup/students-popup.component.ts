7c62a8583df2c42426052a2c400a119fb5d10e32
AdrianS2002 <aschiop2002@yahoo.com>
2025-03-21 17:10:37 +0200

    changed ngOnInit into ngOnChnges

--- src/app/courses/students-popup/students-popup.component.ts
+++ src/app/courses/students-popup/students-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatabaseService } from '../../database/database.service';
 import { User } from '../../database/models/user.model';
@@ -12,7 +12,7 @@ import { NgFor } from '@angular/common';
   templateUrl: './students-popup.component.html',
   styleUrl: './students-popup.component.css'
 })
-export class StudentsPopupComponent implements OnInit {
+export class StudentsPopupComponent  {
   @Input() courseId!: string;
   @Output() close = new EventEmitter<void>();
   successMessage: string | null = null;
@@ -22,22 +22,21 @@ export class StudentsPopupComponent implements OnInit {
 
   constructor(private dbService: DatabaseService) {}
 
-  ngOnInit() {
-    // Încarcă toți studenții
-    this.dbService.getAllUsers()
-      .pipe(take(1))
-      .subscribe(users => {
-        this.students = users.filter(u => u.roles.includes('STUDENT'));
-      });
-  
-    // Încarcă enrollments pentru acest curs
-    this.dbService.getEnrollmentsForCourse(this.courseId)
-      .pipe(take(1))
-      .subscribe(enrollments => {
-        this.enrolledStudent = enrollments.map(e => e.studentId);
-      });
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['courseId'] && this.courseId) {
+      this.loadStudents();
+    }
   }
-  
+  loadStudents() {
+    this.dbService.getAllUsers().subscribe(users => {
+      this.students = users.filter(u => u.roles.includes('STUDENT'));
+    });
+
+    this.dbService.getEnrollmentsForCourse(this.courseId).subscribe(enrollments => {
+      this.enrolledStudent = enrollments.map(e => e.studentId);
+    });
+  }
+ 
 
   enrollStudent(studentId: string) {
     this.dbService.enrollStudent(this.courseId, studentId).subscribe({
