40073792a354ab60dc893397fa2fb29dad025112
AdrianS2002 <aschiop2002@yahoo.com>
2025-04-15 16:04:32 +0300

    added search bar functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { User } from '../database/models/user.model';
+import { DatabaseService } from '../database/database.service';
+import { Course } from '../database/models/course.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, NgFor, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -16,7 +19,15 @@ export class HeaderComponent implements OnInit, OnDestroy {
   isLoggedIn: boolean = false;
   private authSubscription!: Subscription;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  searchQuery = '';
+  allCourses: Course[] = [];
+  filteredCourses: Course[] = [];
+
+  constructor(private authService: AuthService, private router: Router, private dbService: DatabaseService) { 
+    this.dbService.getAllCourses().subscribe(courses => {
+      this.allCourses = courses;
+    });
+  }
 
   ngOnInit() {
     this.authSubscription = this.authService.user.subscribe((user: User | null) => {
@@ -50,5 +61,22 @@ export class HeaderComponent implements OnInit, OnDestroy {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
     }
+  }
+  onSearchChange() {
+    const query = this.searchQuery.toLowerCase().trim();
+    if (query.length === 0) {
+      this.filteredCourses = [];
+      return;
+    }
+
+    this.filteredCourses = this.allCourses.filter(course =>
+      course.title.toLowerCase().includes(query)
+    );
+  }
+
+  navigateToCourse(courseId: string) {
+    this.router.navigate(['/courses', courseId]);
+    this.filteredCourses = [];
+    this.searchQuery = '';
   }
 }
